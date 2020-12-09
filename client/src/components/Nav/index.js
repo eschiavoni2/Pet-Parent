83475edab7a86ad09dd64c9914ee480d2fe2c4fb
Erika <e.m.zibelnik@gmail.com>
2020-12-09 14:40:02 -0500

    close #3 changed jumbotron photo and removed margin

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -42,7 +42,7 @@ function Nav() {
 
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
         Pet Pals
         </Link>
