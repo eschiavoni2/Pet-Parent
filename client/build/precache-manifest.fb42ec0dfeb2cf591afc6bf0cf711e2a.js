e1168990f860a15674224662621c23e356a42b9e
Erika <e.m.zibelnik@gmail.com>
2020-12-16 12:20:08 -0500

    deploy

---
+++ client/build/precache-manifest.fb42ec0dfeb2cf591afc6bf0cf711e2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcf4bf82fe34f8e1bacf5c1a242b3ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d168a15dfa2447f001d0",
+    "url": "/static/css/2.af9ebb16.chunk.css"
+  },
+  {
+    "revision": "34c55b342eb26dc50b61",
+    "url": "/static/css/main.3e7d5aeb.chunk.css"
+  },
+  {
+    "revision": "d168a15dfa2447f001d0",
+    "url": "/static/js/2.c3652d7a.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.c3652d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c55b342eb26dc50b61",
+    "url": "/static/js/main.ca86ee9d.chunk.js"
+  },
+  {
+    "revision": "20c573aaaa2d44c1c4a3",
+    "url": "/static/js/runtime-main.5908aa69.js"
+  }
+]);
